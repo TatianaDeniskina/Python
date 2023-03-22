625b584a00d3e80266f79bd3fa4309570e4999f6
TatianaDeniskina <119048601+TatianaDeniskina@users.noreply.github.com>
2023-03-22 10:08:42 +0300

    Add solution for seminar 7

---
+++ Sem1Task2.py
@@ -0,0 +1,28 @@
+class Road:
+    _surface_spec_gravity: float = 0.02
+
+    def __init__(self, length: [int, float], width: [int, float]):
+        self._length = float(length)
+        self._width = float(width)
+
+    def get_surface_gravity(self, thickness: float) -> [float, None]:
+        try:
+            return (self._length * self._width
+                    * thickness * self._surface_spec_gravity)
+        except TypeError:
+            return None
+
+
+if __name__ == '__main__':
+    try:
+        x = input('Введите длинну дороги в метрах: ')
+        y = input('Введите ширину дороги в метрах: ')
+        road = Road(x, y)
+        print(
+            'Масса дорожного покрытия составит:',
+            road.get_surface_gravity(20),
+            'тонн'
+        )
+    except TypeError:
+        print('класс Road требует 2 позиционных аргумента')
+
